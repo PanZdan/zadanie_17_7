c91148b7b590b51ea50afc0634c9ce8bc9eddead
Konrad Zdanowski <konrad-zdanowski@wp.pl>
2018-02-26 22:44:57 +0100

    Added first versions project

---
+++ config.js
@@ -0,0 +1,7 @@
+var express = require('express');
+
+module.exports = {
+  'GOOGLE_CLIENT_ID': '1054316200131-vjg3gqq61m34iraq3e7tncostfb8pia4.apps.googleusercontent.com',
+  'GOOGLE_CLIENT_SECRET': 'gRn2bnk-aOGMAY8HT6ghUZ0m',
+  'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
